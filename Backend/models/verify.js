13fa008409ec9a74322f18905d32750ef8c4a89d
krutagya <krutagya.kaneria.cg@gmail.com>
2025-03-22 11:45:51 +0530

    schema of verification in the models as per exidraw

---
+++ Backend/models/verify.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+// details verification ki or sath me productid mil jayegi so we can target 
+
+const verifySchema = mongoose.Schema({
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    productCertificate: { type: Array },
+    otherDetails: { type: String }
+});
+
+export const Verify = mongoose.model("Verify", verifySchema);
